5e03fa87e312081e5f6c6e3bf4a366aaf080d5cf
FedFedavi <kykypyk@gmail.com>
2024-08-19 20:48:00 +0400

    remake data

---
+++ main.py
@@ -0,0 +1,17 @@
+<<<<<<< Updated upstream
+=======
+data = [
+    ['100', '200', '300'],
+    ['400', '500', '600']
+    ]
+
+numbers = []
+
+for row in data:
+    for text in row:
+        number = int(text)
+        numbers.append(number)
+
+print(data)
+print(numbers)
+>>>>>>> Stashed changes
